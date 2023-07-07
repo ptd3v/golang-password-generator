3bb9019580b3e44b88e321ed9244166c67f5e3ce
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-07-07 03:58:32 +0100

    Math/rand function added

--- golang-password-generator.go
+++ golang-password-generator.go
@@ -1,22 +1,37 @@
-//Main entry to the program
+// Main entry to the program
 package main
 
 import (
-	//Import strong random number generation tool
-	"crypto/rand"
-	//Import format functions for formatted output 
+	//A random number generator.
+	"math/rand"
+	//An alterative for a more secure random number generator would be "crypto/rand".
+	//crypto/rand
+	//Provides format functions for formatted output e.g fmt.Println
 	"fmt"
 )
 
-//Constant values
+// Constant values
 const (
-	//Password character sets to use. Identifier = value. 
+	//Password character sets to use. Identifier = value.
 	uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 	digits           = "0123456789"
 	specialChars     = "!@#$%^&*()_+-=[]{}|;:,.<>?/~"
 )
 
-func generatePassword{
-	
+// Create a blank password string
+func generatePassword() string {
+	//Concatenate all characters into one unified charset
+	charset := uppercaseLetters + lowercaseLetters + digits + specialChars
+	//Initialize an empty string to store the generated password
+	password := ""
+
+	//Generate a password of length 20
+	for i := 0; i < 20; i++ {
+		//Randomly select a character from the charset and append it to the password string
+		password += string(charset[rand.Intn(len(charset))])
+	}
+
+	//Return the generated password to the string
+	return password
 }
